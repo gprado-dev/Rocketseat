f06cce9e5b96b82955828a3c0b1ef5f16a1bb7ab
Gustavo Prado <contato.gprado@gmail.com>
2023-10-19 09:42:45 -0300

    Explorer 17: Finished project

---
+++ 2.Explorer/17-focus-timer/assets/js/main.js
@@ -0,0 +1,4 @@
+import './modules/toggle-mode.js'
+import * as FocusTimer from './modules/focusTimer/index.js'
+
+FocusTimer.start(1, 0)
---
+++ 2.Explorer/17-focus-timer/assets/js/modules/focusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/audios/button-press.wav')
+
+export const buttonKitchenTimer = new Audio('./assets/audios/kitchen-timer.mp3')
+
+export const bgAudio = new Audio('./assets/audios/bg-audio.mp3')
+
+bgAudio.loop = true
