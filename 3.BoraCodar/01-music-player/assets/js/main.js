039cec23516f509ed860ae0d5538a117d78fe007
Gustavo Prado <contato.gprado@gmail.com>
2023-10-26 11:17:30 -0300

    Separate search bar in modules

--- 3.BoraCodar/01-music-player/assets/js/main.js
+++ 3.BoraCodar/01-music-player/assets/js/main.js
@@ -1,5 +1,6 @@
 // responsiveness 
 let device = document.querySelector('html')
+let body = document.querySelector('body')
 let vw = window.matchMedia('(max-width: 700px)').matches
 
 if(vw) {
@@ -13,13 +14,49 @@ if(vw) {
 
 // clear placeholder inputs when focus
   let inputSearch = document.getElementById('search')
+  let placeholderMessage = 'Buscar na playlist'
 
-  inputSearch.addEventListener("focus", () => {
-  inputSearch.placeholder = '';
-  });
+  inputSearch.placeholder = placeholderMessage;
 
-  inputSearch.addEventListener("blur", () => {
-  inputSearch.placeholder = 'Encontrar nesta página';
-  });
+  if (device.classList[0] == 'desktop') {
+    inputSearch.addEventListener("focus", () => {
+    inputSearch.placeholder = '';
+    });
+
+    inputSearch.addEventListener("blur", () => {
+    inputSearch.placeholder = placeholderMessage;
+    });
+  }
+
+
+  // toggle visibility search button
+  // let buttonSearch = document.getElementById('button-search')
+  // let iconSearch = document.getElementById('icon-search')
+
+  // buttonSearch.addEventListener("click", () => {
+  //   buttonSearch.classList.toggle('visibility-off')
+
+  //   inputSearch.classList.toggle('visibility-off')
+  //   inputSearch.classList.toggle('visibility-on')
+
+  //   iconSearch.classList.toggle('visibility-off')
+
+  //   inputSearch.focus()
+  // })
+
+  // if  (inputSearch.classList.contains('visibility-on')) {
+  //   console.log('abriu')
+  // }
+
+
+
+
+
+  // if (!inputSearch.focus()) {
+  //   console.log('sem foco')
+  // }
+
+
+
 
   console.log(window.innerWidth)
