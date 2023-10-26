9cffec82cd62d45550a94bc08e62f46ea779a6ff
Gustavo Prado <contato.gprado@gmail.com>
2023-10-25 21:51:11 -0300

    Start dev controlers bar

--- 3.BoraCodar/01-music-player/assets/js/main.js
+++ 3.BoraCodar/01-music-player/assets/js/main.js
@@ -1,3 +1,4 @@
+// responsiveness 
 let device = document.querySelector('html')
 let vw = window.matchMedia('(max-width: 700px)').matches
 
@@ -8,3 +9,15 @@ if(vw) {
   device.classList.remove('mobile')
   device.classList.add('desktop')
 }
+
+
+// clear placeholder inputs when focus
+let inputSearch = document.getElementById('search')
+
+document.ready(function() {
+  "use strict";
+  ("inputSearch").on("focus",function(){
+    inputSearch.placeholder = '';
+  });
+});
+
