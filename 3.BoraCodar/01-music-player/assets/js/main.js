b17789ccc5e984e7f04c1e5e832be6dd7ce8171f
Gustavo Prado <contato.gprado@gmail.com>
2023-10-29 09:54:38 -0300

    BoraCodar 01: Element refactoring
    Input-search element for desktop app.

--- 3.BoraCodar/01-music-player/assets/js/main.js
+++ 3.BoraCodar/01-music-player/assets/js/main.js
@@ -1,3 +1,3 @@
 import * as Global from './modules/global/index.js'
-import * as inputSearch from './modules/playlist/search-input/index.js'
+import * as inputSearch from './modules/playlist/input-search/index.js'
 import * as Controler from './modules/playlist/controlers/index.js'
---
+++ 3.BoraCodar/01-music-player/assets/js/modules/playlist/input-search/toggle-search-button.js
@@ -0,0 +1,23 @@
+import * as Element from './elements.js'
+
+
+export function toggleSearchButtonOnClick() {
+  Element.toggleSearchButton.addEventListener("click", () => {
+    
+    Element.toggleSearchButton.classList.toggle('invisible');
+    Element.iconSearch.classList.toggle('invisible')
+  })
+}
+
+export function toggleSearchButtonOnBlur() {
+  if (Element.inputSearch.value =="") {
+    Element.inputSearch.addEventListener("blur", () => {
+      
+      Element.iconSearch.classList.toggle('invisible')
+      Element.toggleSearchButton.classList.toggle('invisible');
+    });
+  }
+}
+
+toggleSearchButtonOnClick()
+toggleSearchButtonOnBlur()
