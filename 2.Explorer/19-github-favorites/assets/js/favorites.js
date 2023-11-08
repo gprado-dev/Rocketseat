3405b81670b79fb3df57b6f55621222b3910d1be
Gustavo Prado <contato.gprado@gmail.com>
2023-11-08 01:37:59 -0300

    Explorer 19: Finished JavaScript

--- 2.Explorer/19-github-favorites/assets/js/favorites.js
+++ 2.Explorer/19-github-favorites/assets/js/favorites.js
@@ -1,13 +1,47 @@
+import { GithubUser } from "./GithubUser.js"
+
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
+
+    GithubUser.search('gprado-dev')
+    .then(user => console.log(user))
   }
 
   load() {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if (userExists) {
+        throw new Error('Usuário já cadastrado')
+      }
+
+      const user = await GithubUser.search(username)
+  
+      if (user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    }
+    catch(error) {
+      alert(error.message)
+    }
   }
 
   delete(user) {
@@ -16,6 +50,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -28,6 +63,16 @@ export class FavoritesView extends Favorites {
 
 
     this.update()
+    this.onadd()
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
   }
 
   update() {
@@ -44,6 +89,9 @@ export class FavoritesView extends Favorites {
       
       // name
       row.querySelector('.user p').textContent = user.name
+
+      // link
+      row.querySelector('.user a').href = `https://github.com/${user.login}`
 
       // login
       row.querySelector('.user span').textContent = user.login
